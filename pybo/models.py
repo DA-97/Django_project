7fc02942f29e2ee0c3a0bbc4e4f70fa2450a6d4b
dong <dong@gmail.com>
2021-06-11 15:49:48 +0900

    3-08 ~ 3-09 comp

--- pybo/models.py
+++ pybo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Question(models.Model):
+    modify_date = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
@@ -13,7 +14,16 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    modify_date = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
